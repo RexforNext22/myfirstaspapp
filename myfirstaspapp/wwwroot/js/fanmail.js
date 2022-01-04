144d00d94bd6cff16323c157cbfbd21fda799e53
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-01-04 13:44:03 -0700

    Initial check-in of module myfirstaspapp

---
+++ myfirstaspapp/wwwroot/js/fanmail.js
@@ -0,0 +1,10 @@
+﻿$("#btnsend").click(function () {
+
+    sFrom = $("#from").val();
+    sSubject = $("#subject").val();
+    sMessage = $("#message").val();
+
+    alert("From: " + sFrom + ", Subject: " + sSubject + ", Message: " + sMessage);
+
+    $("#picand").fadeOut("slow")
+})
